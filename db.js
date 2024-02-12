96ae524319c4fe66c6e115bde1ccb86e91b6a910
Luis Hernandez <sarco_luis@hotmail.com>
2024-02-12 09:49:43 -0600

    Se crearon los componentes de productos, categorias y clientes

--- db.js
+++ db.js
@@ -3,9 +3,15 @@ const funcdb = ()=>{
     let indexDB = indexedDB.open('db_sistema',1);
     indexDB.onupgradeneeded = e=>{
         let req = e.target.result,
-            tblproducto = req.createObjectStore('productos',{keyPath:'idProducto'});
+            tblproducto = req.createObjectStore('productos',{keyPath:'idProducto'}),
+            tblcategoria = req.createObjectStore('categorias',{keyPath:'idCategoria'}),
+            tblcliente = req.createObjectStore('clientes',{keyPath:'idCliente'});
         tblproducto.createIndex('idProducto','idProducto',{unique:true});
         tblproducto.createIndex('codigo','codigo',{unique:true});
+        tblcategoria.createIndex('idCategoria','idCategoria',{unique:true});
+        tblcategoria.createIndex('codigo','codigo',{unique:true});
+        tblcliente.createIndex('idCliente','idCliente',{unique:true});
+        tblcliente.createIndex('codigo','codigo',{unique:true});
     };
     indexDB.onsuccess = e=>{
         db = e.target.result;
