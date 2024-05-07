2934b293ac80b61ea05a22b46e8a96da6d4577b0
pavillalta <Estudiante@DESKTOP-J6D5H3A>
2024-05-07 09:50:09 -0600

    iniciamos con el crud node + mongo + express + socket

--- node/server.js
+++ node/server.js
@@ -1,12 +1,33 @@
-const http = require('http'),
-    server = http.createServer((req, res)=>{
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        
-        res.writeHead(200, {'content-type':'text/plain'});
-        res.end('Saludo');
-    });
-server.listen(3000, res=>{
-    console.log("Server ejecutandose en puerto 3000");
+const express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socked.io')(http,{
+        allowEIO3:true,
+        cors:{
+            origin:['http://localhost'],
+            credential:true
+        }
+    }),
+    { MongoClient } = require('mongodb'),
+    url = 'mongodb://localhost:27017',
+    client = new MongoClient(url),
+    dbname = 'chatUGB',
+    port = 3001;
+app.use(express.json());
+
+async function conectarMongoDb(){
+    await client.connect();
+    return client.db(dbname);
+}
+app.get('/', (req, resp)=>{
+    resp.sendFile(__dirname + '/index.html');
+});
+app.post('/usuarios/guardar',async(req, resp)=>{
+    let db = await conectarMongoDb(),
+        collection=db.collection('usuarios');
+    collection.insertOne(req.body);
+    resp.send(req.body);
+});
+http.listen(port, event=>{
+    console.log("Server ejecutado en el puerto", port);
 });
